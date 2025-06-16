b59b387f7cf34f2732916bc1feb3cbe08b4287a9
Sathya pramod <sathyapramod12@gmail.com>
2025-06-16 21:03:25 +0530

    updated gitignore file and added Chapter of list

---
+++ Chapter_3_Lists/bicycles.py
@@ -0,0 +1,16 @@
+"""
+A list is a collection of items in a particular order. You can make a list that
+includes the letters of the alphabet, the digits from 0 to 9, or the names of
+all the people in your family.
+"""
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+"""
+Accessing Elements in a List
+Lists are ordered collections, so you can access any element in a list by
+telling Python the position, or index, of the item desired.
+"""
+print(bicycles[0])
+print(bicycles[3])
