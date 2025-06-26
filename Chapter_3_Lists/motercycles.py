1c0cd7fea7776337a7b0419449e82da2fbc2cd93
Sathya pramod <sathyapramod12@gmail.com>
2025-06-26 11:25:25 +0530

    updated list

--- Chapter_3_Lists/motercycles.py
+++ Chapter_3_Lists/motercycles.py
@@ -2,5 +2,37 @@
 Modifying, Adding, and Removing Elements
 """
 
+# Modifying Elements in a List
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
+
+print(motorcycles[0])
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+# Adding Elements to a List
+# Appending Elements to the End of a List -- "The simplest way to add a new element to a list is to append the item to the
+# list. When you append an item to a list, the new element is added to the end
+# of the list."
+
+motorcycles.append('honda')
+print(motorcycles)
+
+# The append() method makes it easy to build lists dynamically. For example,
+# you can start with an empty list and then add items to the list using a series
+# of append() calls.
+
+cars = []
+cars.append('hyundai')
+cars.append('nexa')
+cars.append('tata')
+cars.append('suzuki')
+print(cars)
+
+# Inserting Elements into a List
+# You can add a new element at any position in your list by using the insert()
+# method. Below example we insert the item at the beginning of the list.
+
+cars.insert(0, 'kia')
+print(cars)
