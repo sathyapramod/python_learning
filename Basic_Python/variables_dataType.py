acb6ddea1b1036c2777a031b8c971bdff9324d45
Sathya Pramod D S <sathya.pramod@komprise.com>
2025-07-01 21:38:00 +0530

    Update variables_dataType.py

--- Basic_Python/variables_dataType.py
+++ Basic_Python/variables_dataType.py
@@ -1,11 +1,12 @@
 # Day 1: Variables, Data Types & Operators
 
+# 1. Variables
 name = "Alice"
 age = 30
 height = 1.75
 is_student = True
 
-
+# 2. Data Types
 num_int = 10         # int
 num_float = 20.5     # float
 my_string = "Hello, Python!" # str
